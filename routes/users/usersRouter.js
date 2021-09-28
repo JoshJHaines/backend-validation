5488628628e472deeebd7a813f721587853c7dd2
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-28 15:46:23 -0400

    imported deleteUserById from controller and created delete route

--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-const { createUser, getAllUser } = require("./controller/userController")
+const { createUser, getAllUser, deleteUserById } = require("./controller/userController")
 /* GET users listing. */
 
 router.get("/", getAllUser)
 
 router.post('/create-user', createUser );
+
+router.delete("/delete-user-by-id/:id", deleteUserById)
 
 module.exports = router;
