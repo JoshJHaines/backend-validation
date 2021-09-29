7c472a894c9af43859ce48bf57d73118efef9063
JoshuaJHaines <joshjhaines3@gmail.com>
2021-09-29 14:06:45 -0400

    added character validation on first name

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -1,19 +1,37 @@
 const User = require("../model/User");
 
-async function getAllUser(req, res) {
-    try {
-        let fetchedUser = await User.find({});
-
-        res.json({ message: "success", payload: fetchedUser });
-    } catch (error) {
-        res.status(500).json({
-            message: "you have failed",
-            error: error.message,
-        });
+function checkForNumbersAndSymbol(target){
+    if(target.match(/[!`\-=@#$%^&*()\[\],.?":;{}|<>1234567890]/g)){
+        return true
+    } else{ 
+        return false
     }
+}
+
+async function getAllUser(req, res) {
+	try {
+		let fetchedUser = await User.find({});
+
+		res.json({ message: "success", payload: fetchedUser });
+	} catch (error) {
+		res.status(500).json({
+			message: "you have failed",
+			error: error.message,
+		});
+	}
 }
 async function createUser(req, res) {
 	const { firstName, lastName, username, email, password } = req.body;
+
+    console.log('Check First: ', checkForNumbersAndSymbol(firstName))
+    console.log('Check Last: ', checkForNumbersAndSymbol(lastName))
+	if (checkForNumbersAndSymbol(firstName)) {
+		return res.status(500).json({
+			message: "error",
+			error: "first name cannot contain special characters and numbers",
+		});
+	}
+
 	try {
 		const createdUser = new User({
 			firstName,
@@ -34,7 +52,7 @@ async function deleteUserById(req, res) {
 	try {
 		let deletedUser = await User.findByIdAndDelete(req.params.id);
 
-        res.json({ message: "success", payload: deletedUser });
+		res.json({ message: "success", payload: deletedUser });
 	} catch (error) {
 		res.status(500).json({
 			message: "you have failed",
@@ -43,9 +61,8 @@ async function deleteUserById(req, res) {
 	}
 }
 
-
 module.exports = {
-    getAllUser,
+	getAllUser,
 	createUser,
-    deleteUserById
+	deleteUserById,
 };
