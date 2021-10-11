8bf938e058277e914b93476e85fddd46b1ee4b62
JoshuaJHaines <joshjhaines3@gmail.com>
2021-10-11 14:29:11 -0400

    adding more files that I missed in class

--- routes/users/controller/userController.js
+++ routes/users/controller/userController.js
@@ -1,57 +1,10 @@
 const bcrypt = require("bcryptjs");
-var jwt = require('jsonwebtoken');
-
-const {
-	isEmpty,
-	isAlpha,
-	isAlphanumeric,
-	isEmail,
-	isStrongPassword,
-} = require("validator");
-
+var jwt = require("jsonwebtoken");
 const User = require("../model/User");
-
+const errorHandler = require("../../utils/errorHandler/errorHandler");
 
 async function createUser(req, res) {
 	const { firstName, lastName, username, email, password } = req.body;
-	let body = req.body;
-	let errObj = {};
-
-	for (let key in body) {
-		if (isEmpty(body[key])) {
-			errObj[`${key}`] = `${key} cannot be empty`;
-		}
-	}
-
-	if (!isAlpha(firstName)) {
-		errObj.firstName =
-			"First Name cannot have special characters or numbers";
-	}
-
-	if (!isAlpha(lastName)) {
-		errObj.lastName = "Last Name cannot have special characters or numbers";
-	}
-
-	if (!isAlphanumeric(username)) {
-		errObj.username = "Username cannot have special characters";
-	}
-
-	if (!isEmail(email)) {
-		errObj.email = "please enter a valid email";
-	}
-
-	if (!isStrongPassword(password)) {
-		errObj.password =
-			"Your password must contain 1 lowercase, 1 uppercase, 1 number, 1 special character and at least 8 characters long";
-	}
-
-	if (Object.keys(errObj).length > 0) {
-		//How would you validate firstName to make sure only alphabet is allowed
-		return res.status(500).json({
-			message: "error",
-			error: errObj,
-		});
-	}
 
 	try {
 		let salt = await bcrypt.genSalt(10);
@@ -69,33 +22,17 @@ async function createUser(req, res) {
 
 		res.json({ message: "success", payload: savedUser });
 	} catch (error) {
-		res.status(500).json({ message: "error", error: error.message });
+		res.status(500).json({ message: "error", error: errorHandler(error) });
 	}
 }
 
+//not in class
 async function login(req, res) {
 	const { email, password } = req.body;
-
-	let errObj = {};
-
-	if (isEmpty(password)) {
-		errObj.password = "password cannot be empty";
-	}
-
-	if (isEmpty(email)) {
-		errObj.email = "email cannot be empty";
-	}
-
-	if (!isEmail(email)) {
-		errObj.email = "please enter a valid email";
-	}
-
-	if (Object.keys(errObj).length > 0) {
-		return res.status(500).json({
-			message: "error",
-			error: errObj,
-		});
-	}
+	//log the user in using email and password
+	//if email does not exists, error message "please go sign up"
+	//if email exists but wrong password error message "please check your email and password"
+	//if successful - for now send message "Login success"
 
 	try {
 		let foundUser = await User.findOne({ email: email });
@@ -116,17 +53,16 @@ async function login(req, res) {
 					error: "Please check your email and password",
 				});
 			} else {
-				
 				let jwtToken = jwt.sign(
 					{
 						email: foundUser.email,
-						username: foundUser.username
+						username: foundUser.username,
 					},
 					process.env.JWT_SECRET,
-					{ expiresIn: "24h"}
-				)
+					{ expiresIn: "24h" }
+				);
 
-				res.json({ message: "success", payload: jwtToken})
+				res.json({ message: "success", payload: jwtToken });
 			}
 		}
 	} catch (e) {
@@ -134,9 +70,6 @@ async function login(req, res) {
 	}
 }
 
-
-
-//not in class
 async function getAllUser(req, res) {
 	try {
 		let fetchedUser = await User.find({});
